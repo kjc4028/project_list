b4fdfab17a3ea6665f3e6fe7767d3c0680a45a5b
kjc4028 <whd4028@gmail.com>
2019-10-02 23:53:29 +0900

    spring boot mvc setting

---
+++ dev_web-1/src/test/java/com/dev/DevWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
